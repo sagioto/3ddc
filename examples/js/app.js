e617949dea732940bebcca41a14b1671645ad038
ogadiel <ogadiel@vmware.com>
2015-12-23 22:01:03 +0200

    css

---
+++ examples/js/app.js
@@ -0,0 +1,20 @@
+$(function () {
+
+
+    //AJAX to server to get all Data Center names
+    $.ajax({
+        type: "GET",
+        url: "/app",
+    }).done(function (res){
+        //append each data center to navigation list
+    });
+
+
+
+
+
+
+
+
+
+};
