906e73210550ddaacdc18becd99e481b0813af32
ogadiel <ogadiel@vmware.com>
2015-12-23 22:26:47 +0200

    combined background pattern

--- examples/js/app.js
+++ examples/js/app.js
@@ -4,7 +4,7 @@ $(function () {
     //AJAX to server to get all Data Center names
     $.ajax({
         type: "GET",
-        url: "/app",
+        url: "/app"
     }).done(function (res){
         //append each data center to navigation list
     });
