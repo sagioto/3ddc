c08e221625ca7f1b033650d82a62dc7a4dca9587
asegev <asegev@vmware.com>
2015-12-24 00:40:40 +0200

    vm can be positioned on top of other vm

--- examples/demo/objects/baseEntity.js
+++ examples/demo/objects/baseEntity.js
@@ -1,5 +1,6 @@
 function BaseEntity(config){
     this.type = config.type;
+    this.isBeingDragged = false;
     this.status = config.status;
     this.name = config.name;
     this.receiveShadow = true;
@@ -10,14 +11,38 @@ function BaseEntity(config){
 
 BaseEntity.prototype = Object.create(Physijs.BoxMesh.prototype);
 
+BaseEntity.prototype.setIsBeingDragged = function(isBeingDragged){
+    this.isBeingDragged = isBeingDragged;
+}
 BaseEntity.prototype.afterObjectCreated = function(){
-    this.addEventListener( 'collision', function( other_object, relative_velocity, relative_rotation, contact_normal ) {
-        // `this` has collided with `other_object` with an impact speed of `relative_velocity` and a rotational force of `relative_rotation` and at normal `contact_normal`
-        var v = new THREE.Vector3( 0, 0, 0 );//_vector.set( 0, 0, 0 );
-        other_object.setAngularFactor( v );
-        other_object.setAngularVelocity( v );
-        other_object.setLinearFactor( v );
-        other_object.setLinearVelocity( v );
+    this.addEventListener( 'collision', this.onCollision);
+}
+BaseEntity.prototype.freezeObject = function(){
+    var v = new THREE.Vector3( 0, 0, 0 );//_vector.set( 0, 0, 0 );
+    this._changeVelocityAndFactors( v );
+}
+BaseEntity.prototype.resumeObjectMovement = function(){
+    var v = new THREE.Vector3( 1, 1, 1 );//_vector.set( 0, 0, 0 );
+    this._changeVelocityAndFactors( v );
+}
+BaseEntity.prototype._changeVelocityAndFactors = function(v){
+    this.setAngularFactor( v );
+    this.setAngularVelocity( v );
+    this.setLinearFactor( v );
+    this.setLinearVelocity( v );
+}
+
+
+BaseEntity.prototype.onCollision = function(other_object, relative_velocity, relative_rotation, contact_normal){
+
+
+    if(this.isBeingDragged && Math.abs(this.position.y-other_object.position.y)>0.05 ){
+        other_object.freezeObject(  );
+        this.resumeObjectMovement( );
+        this.position.set( this.position.x, other_object.position.y+VM.prototype.height+0.1, this.position.z );
+        this.__dirtyPosition = true;
+        this.freezeObject( );
+    }
+
         //this.
-    });
 }
