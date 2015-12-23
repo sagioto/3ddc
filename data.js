a96020c75b45fe5b0d02ad6fc8fa428fc4fa71cb
Sagi Bernstein <sbernstein@vmware.com>
2015-12-23 13:07:36 +0200

    changed data to be inventory declaration

---
+++ data.js
@@ -0,0 +1,288 @@
+var inventory = {
+  "datacenters" : [
+    {
+      "name" : "DC1",
+      "servers" : [
+        {
+          "name" : "Server11",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm111",
+              "status" : "online"
+            },
+            {
+              "name" : "vm112",
+              "status" : "online"
+            },
+            {
+              "name" : "vm113",
+              "status" : "online"
+            },
+            {
+              "name" : "vm114",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server12",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm121",
+              "status" : "online"
+            },
+            {
+              "name" : "vm122",
+              "status" : "online"
+            },
+            {
+              "name" : "vm123",
+              "status" : "online"
+            },
+            {
+              "name" : "vm124",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server13",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm131",
+              "status" : "online"
+            },
+            {
+              "name" : "vm132",
+              "status" : "online"
+            },
+            {
+              "name" : "vm133",
+              "status" : "online"
+            },
+            {
+              "name" : "vm134",
+              "status" : "online"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name" : "DC2",
+      "servers" : [
+        {
+          "name" : "Server21",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm211",
+              "status" : "online"
+            },
+            {
+              "name" : "vm212",
+              "status" : "online"
+            },
+            {
+              "name" : "vm213",
+              "status" : "online"
+            },
+            {
+              "name" : "vm214",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server22",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm221",
+              "status" : "online"
+            },
+            {
+              "name" : "vm222",
+              "status" : "online"
+            },
+            {
+              "name" : "vm223",
+              "status" : "online"
+            },
+            {
+              "name" : "vm224",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server23",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm231",
+              "status" : "online"
+            },
+            {
+              "name" : "vm232",
+              "status" : "online"
+            },
+            {
+              "name" : "vm233",
+              "status" : "online"
+            },
+            {
+              "name" : "vm234",
+              "status" : "online"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name" : "DC3",
+      "servers" : [
+        {
+          "name" : "Server31",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm311",
+              "status" : "online"
+            },
+            {
+              "name" : "vm312",
+              "status" : "online"
+            },
+            {
+              "name" : "vm313",
+              "status" : "online"
+            },
+            {
+              "name" : "vm314",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server32",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm321",
+              "status" : "online"
+            },
+            {
+              "name" : "vm322",
+              "status" : "online"
+            },
+            {
+              "name" : "vm323",
+              "status" : "online"
+            },
+            {
+              "name" : "vm324",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server33",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm331",
+              "status" : "online"
+            },
+            {
+              "name" : "vm332",
+              "status" : "online"
+            },
+            {
+              "name" : "vm333",
+              "status" : "online"
+            },
+            {
+              "name" : "vm334",
+              "status" : "online"
+            }
+          ]
+        }
+      ]
+    },
+    {
+      "name" : "DC4",
+      "servers" : [
+        {
+          "name" : "Server41",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm411",
+              "status" : "online"
+            },
+            {
+              "name" : "vm412",
+              "status" : "online"
+            },
+            {
+              "name" : "vm413",
+              "status" : "online"
+            },
+            {
+              "name" : "vm414",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server42",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm421",
+              "status" : "online"
+            },
+            {
+              "name" : "vm422",
+              "status" : "online"
+            },
+            {
+              "name" : "vm423",
+              "status" : "online"
+            },
+            {
+              "name" : "vm424",
+              "status" : "online"
+            }
+          ]
+        },
+        {
+          "name" : "Server43",
+          "status" : "online",
+          "vms" : [
+            {
+              "name" : "vm431",
+              "status" : "online"
+            },
+            {
+              "name" : "vm432",
+              "status" : "online"
+            },
+            {
+              "name" : "vm433",
+              "status" : "online"
+            },
+            {
+              "name" : "vm434",
+              "status" : "online"
+            }
+          ]
+        }
+      ]
+    }
+  ]
+};
